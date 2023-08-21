346d13d3c13f9e2bffa16735a7272d5e9963d852
MrQuansy <1051622353@qq.com>
2023-08-21 23:44:57 +0800

    syscall lab

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -5,7 +5,7 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
-
+#include "sysinfo.h"
 uint64
 sys_exit(void)
 {
@@ -90,4 +90,31 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+uint64
+sys_trace(void)
+{
+    int mask;
+    argint(0, &mask);
+    if(mask < 0)
+        return -1;
+    myproc()->mask = mask;
+    return 0;
+}
+
+uint64
+sys_sysinfo(void)
+{
+    uint64 addr;
+    argaddr(0, &addr);
+    if(addr < 0)
+        return -1;
+    struct proc *p = myproc();
+    struct sysinfo info;
+    info.freemem = freemem();
+    info.nproc = unusedproc();
+    if(copyout(p->pagetable, addr, (char *)&info, sizeof(info)) < 0)
+        return -1;
+    return 0;
 }
